f6fd760917bf2a1a14a7d34500acbad2926bb273
Abraham Samuel <ebubesammy567@gmail.com>
2025-01-17 21:16:54 +0100

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,61 +1,60 @@
 import { Link } from "react-router-dom";
 import { Button } from "../../components/components/ui/button";
+import {vectorImageTwo} from "../images/image"; // Ensure correct import
+import "../styles/footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      <h2>Thanks for connecting with us</h2>
-      <Button asChild>
-        <Link
-          // to="/todos"
-          className="btn text-blue-900 px-6 py-3 rounded-full text-xl font-semibold hover:text-white transition duration-300"
-        >
-          Login for free
-        </Link>
-      </Button>
+    <div className="footer__main_container"
+      style={{
+        background: "linear-gradient(179.09deg, #00f0ff 0.36%, #26968B 51.22%)"
+      }}
+    >
+      <div
+        className="footer__container"
+        style={{
+          // backgroundImage: `url(${vectorImageTwo})`,
+          // backgroundSize: "100% 100%",
+          // height: "50vh",
+          textAlign: "center",
+          position: "relative",
+          // backgroundPosition: "center",
+          // backgroundRepeat: "no-repeat",
+          width: "100%",
+          paddingTop: "30px",
+        }}
+      >
+        <h2>Thanks for connecting with us</h2>
 
-      <h3>FanConnect</h3>
+        <Button asChild>
+          <Link
+            className="btn text-blue-900 px-6 py-3 rounded-full text-xl font-semibold hover:text-white transition duration-300"
+          >
+            Login for free
+          </Link>
+        </Button>
 
-      <div className="social__contact">
-        <p>Connect with us on socials</p>
+        <h3>FanConnect</h3>
 
-        <div className="social__icons">
-          <div className="icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              style={{ fill: "rgb(0, 0, 0)" }} // Fixed style object
-            >
-              <path d="M19.633 7.997c.013.175.013.349.013.523 0 5.325-4.053 11.461-11.46 11.461-2.282 0-4.402-.661-6.186-1.809.324.037.636.05.973.05a8.07 8.07 0 0 0 5.001-1.721 4.036 4.036 0 0 1-3.767-2.793c.249.037.499.062.761.062.361 0 .724-.05 1.061-.137a4.027 4.027 0 0 1-3.23-3.953v-.05c.537.299 1.16.486 1.82.511a4.022 4.022 0 0 1-1.796-3.354c0-.748.199-1.434.548-2.032a11.457 11.457 0 0 0 8.306 4.215c-.062-.3-.1-.611-.1-.923a4.026 4.026 0 0 1 4.028-4.028c1.16 0 2.207.486 2.943 1.272a7.957 7.957 0 0 0 2.556-.973 4.02 4.02 0 0 1-1.771 2.22 8.073 8.073 0 0 0 2.319-.624 8.645 8.645 0 0 1-2.019 2.083z"></path>
-            </svg>
-          </div>
-          <div className="icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              style={{ fill: "rgb(0, 0, 0)" }} // Fixed style object
-            >
-              <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path>
-            </svg>
-          </div>
-          <div className="icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              style={{ fill: "rgb(0, 0, 0)" }} // Fixed style object
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336 9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z"
-              ></path>
-            </svg>
+        <div className="social__contact">
+          <p>Connect with us on socials</p>
+
+          <div className="social__icons">
+            <div className="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path d="M19.633 7.997c.013.175.013.349.013.523 0 5.325-4.053 11.461-11.46 11.461-2.282 0-4.402-.661-6.186-1.809..." />
+              </svg>
+            </div>
+            <div className="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127..." />
+              </svg>
+            </div>
+            <div className="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <path fillRule="evenodd" clipRule="evenodd" d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145..." />
+              </svg>
+            </div>
           </div>
         </div>
       </div>
